42e7a840a677ae56438690de5feccbd7472b339f
agathys <agathys@list.ru>
2020-12-13 13:29:02 +0300

    fix

---
+++ app/wwwroot/js/site.js
@@ -0,0 +1,17 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+
+
+
+function init() {
+    sec = 0;
+    setInterval(tick, 1000);
+}
+
+function tick() {
+    sec++;
+    document.getElementById("timer").
+        childNodes[0].nodeValue = sec;
+}
